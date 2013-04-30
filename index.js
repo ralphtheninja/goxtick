5d8bcde8834be6f12cc4444b3bda75e350ff53e0
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-04-30 11:01:38 +0200

    add support for currency

--- index.js
+++ index.js
@@ -3,15 +3,36 @@
 var EventEmitter = require('events').EventEmitter
 var sockio       = require('socket.io-client')
 
+module.exports.currencies = function () {
+  return {
+      USD: 'USD'
+    , AUD: 'AUD'
+    , CAD: 'CAD'
+    , CHF: 'CHF'
+    , CNY: 'CNY'
+    , DKK: 'DKK'
+    , EUR: 'EUR'
+    , GBP: 'GBP'
+    , HKD: 'HKD'
+    , JPY: 'JPY'
+    , NZD: 'NZD'
+    , PLN: 'PLN'
+    , RUB: 'RUB'
+    , SEK: 'SEK'
+    , SGD: 'SGD'
+    , THB: 'THB'
+  }
+}
+
 module.exports = function (currency) {
-  // TODO: not set or invalid -> default as 'USD'
-  if (!currency) currency = 'USD'
+  if (!currency || 'string' !== typeof currency)
+    currency = 'USD'
 
   var emitter = new EventEmitter()
-  var sock = sockio.connect('https://socketio.mtgox.com/mtgox')
+  var baseUrl = 'https://socketio.mtgox.com/mtgox'
+  var sock = sockio.connect(baseUrl + '?Currency=' + currency)
 
   sock.on('connect', function () {
-    console.log('connected to mtgox')
     sock.on('message', function (data) {
       if ('private' === data.op) {
         var type = data['private']
@@ -20,12 +41,14 @@ module.exports = function (currency) {
     })
   })
 
+  emitter._socketio = sock
+
   return emitter
 }
 
 
 if (!module.parent) {
-  var ticker = module.exports()
+  var ticker = module.exports('USD')
   ticker.on('ticker', function (data) {
     console.log('ticker data:', data)
   })
